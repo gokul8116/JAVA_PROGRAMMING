336abbe42292406aaa149ebd0b19711e8afbcbd0
GOKUL PAL <137334469+gokul8116@users.noreply.github.com>
2024-09-13 20:58:25 +0530

    Add files via upload

---
+++ CreateObject.java
@@ -0,0 +1,14 @@
+public class CreateObject   
+{    
+void show()    
+{    
+System.out.println("Hello, I am Gokul");    
+}    
+public static void main(String[] args)   
+{    
+//creating an object using new keyword   
+CreateObject obj = new CreateObject();   
+//invoking method using the object  
+obj.show();    
+}    
+}   
