140c6371b8a6f287c0ac5c3d1434a5207663bf0d
GOKUL PAL <137334469+gokul8116@users.noreply.github.com>
2024-09-25 22:19:01 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2020;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
