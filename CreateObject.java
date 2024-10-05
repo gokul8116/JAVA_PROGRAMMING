26c02a292f389e655533046fb7414afe93062645
GOKUL PAL <137334469+gokul8116@users.noreply.github.com>
2024-10-05 21:56:26 +0530

    CreateObject.java

--- CreateObject.java
+++ CreateObject.java
@@ -6,7 +6,6 @@ System.out.println("Hello, I am Gokul");
 }    
 public static void main(String[] args)   
 {    
-//creating an object using new keyword   
 CreateObject obj = new CreateObject();   
 //invoking method using the object  
 obj.show();    
